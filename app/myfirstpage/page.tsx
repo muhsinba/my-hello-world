3ebcce5a4c30d5d5c15fe671595023db8c19b2b8
muhsinba <muhsinba@gmail.com>
2026-05-27 20:54:58 +0300

    Initial commit: Next.js tip calculator

---
+++ app/myfirstpage/page.tsx
@@ -0,0 +1,7 @@
+export default function MyFirstPage() {
+  return (
+    <div className="flex flex-1 items-center justify-center bg-sky-200">
+      <h1 className="text-black">My First Page</h1>
+    </div>
+  );
+}
